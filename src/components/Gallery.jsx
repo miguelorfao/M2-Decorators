c9456aa2e993cb789bef273fdf48adaf31b23a98
miguelorfao <69687296+miguelorfao@users.noreply.github.com>
2025-07-31 22:21:34 +0100

    update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,18 +5,29 @@ function Gallery() {
   return (
     <div style={{ textAlign: "center", margin: "20px" }}>
       <h2 className="text-center my-4">Gallery</h2>
-      {images.map((src, i) => (
-        <img
-          key={i}
-          src={src}
-          style={{
-            width: "200px",
-            height: "150",
-            objectFit: "cover",
-            borderRadius: "8px",
-          }}
-        />
-      ))}
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "20px",
+          marginTop: "20px",
+        }}
+      >
+        {" "}
+        {images.map((src, i) => (
+          <img
+            key={i}
+            src={src}
+            style={{
+              width: "200px",
+              height: "150",
+              objectFit: "cover",
+              borderRadius: "8px",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
