5a63a8de95bb938a8d5597b8362dd6907d7f44fc
miguelorfao <69687296+miguelorfao@users.noreply.github.com>
2025-09-07 21:17:35 +0100

    updated on galery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,46 +3,27 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 
 function Gallery() {
   const images = [
-    "./images/flooring.png",
-    "./images/Stairs.png",
-    "./images/insulation.jpg",
+    "./images/livingroom.png",
+    "./images/passage.png",
+    "./images/stairs.png",
   ];
   return (
-    <div style={{ textAlign: "center", margin: "20px" }}>
+    <Container>
       <h2 className="text-center my-4">Gallery</h2>
-
-      <Container>
-        <Row className="d-flex justify-content-center align-items-center gap-2">
-          {images.map((src, i) => (
-            <Col md={2} lg={3} className="text-center mb-4">
-              {" "}
-              <Card
-                style={{ width: "20rem" }}
-                className="border-0 rounded-5 card"
-              >
-                <img
-                  key={i}
-                  style={{ height: "20rem" }}
-                  src={src}
-                  alt={`image + ${i}`}
-                />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          gap: "20px",
-          marginTop: "20px",
-        }}
-      >
-        {" "}
+      <div className="d-flex flex-wrap justify-content-center gap-4">
+        {images.map((src, i) => (
+          <div key={i}>
+            {" "}
+            <Card
+              style={{ width: "25rem" }}
+              className="border-0 rounded-5 card"
+            >
+              <img key={i} src={src} alt={`image + ${i}`} />
+            </Card>
+          </div>
+        ))}
       </div>
-    </div>
+    </Container>
   );
 }
 
